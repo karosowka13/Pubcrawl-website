607822fb7c338c1b4ba874a625498d97ec353369
karosowka13 <sowa@student.agh.edu.pl>
2020-09-24 15:51:54 +0200

    fix bugs and mods

--- app.js
+++ app.js
@@ -1,1 +1,63 @@
-var modalBtn=document.querySelector(".cta-add"),modalBg=document.querySelector(".modal-bg"),modalClose=document.querySelector(".modal-close");modalBtn.addEventListener("click",function(){modalBg.classList.add("bg-active")}),modalClose.addEventListener("click",function(){modalBg.classList.remove("bg-active")}),$("#booknow").on("click",function(){$("#booknow").load("https://fareharbor.com/embeds/api/v1/?autolightframe=yes")}),$("#contact").on("click",function(){$.getScript("headers.php?file=recaptcha.js")}),window.onscroll=function(){myFunction()};var header=document.getElementById("myHeader"),sticky=header.offsetTop;function myFunction(){window.pageYOffset>sticky?header.classList.add("sticky"):header.classList.remove("sticky")}function openTab(e,t){var n,o,c;for(o=document.getElementsByClassName("tabcontent"),n=0;n<o.length;n++)o[n].style.display="none";for(c=document.getElementsByClassName("tablinks"),n=0;n<c.length;n++)c[n].className=c[n].className.replace(" active","");document.getElementById(t).style.display="block",e.currentTarget.className+=" active"}const hamburger=document.querySelector(".hamburger"),navLinks=document.querySelector(".nav-links"),links=document.querySelectorAll(".nav-links li");var mylogo=document.getElementById("myLogo");hamburger.addEventListener("click",()=>{navLinks.classList.toggle("open"),links.forEach(e=>{e.classList.toggle("fade")})}),$(document).ready(function(){$("#hamburgerID").on("click",function(){})}),$(".nav-links li a").on("click",function(){$("#hamburgerID").click()});
+var modalBtn = document.querySelector(".cta-add"),
+	modalBg = document.querySelector(".modal-bg"),
+	modalClose = document.querySelector(".modal-close");
+modalBtn.addEventListener("click", function () {
+	modalBg.classList.add("bg-active");
+}),
+	modalClose.addEventListener("click", function () {
+		modalBg.classList.remove("bg-active");
+	}),
+	$("#booknow")
+		.on("click", function () {
+			$.getScript("https://fareharbor.com/embeds/api/v1/?autolightframe=yes");
+		})
+		.done(function () {
+			window.location.href =
+				"https://fareharbor.com/embeds/book/southtours/items/148437/?full-items=yes&flow=no";
+		});
+$("#contact").on("click", function () {
+	$.getScript("headers.php?file=recaptcha.js");
+}),
+	(window.onscroll = function () {
+		myFunction();
+	});
+var header = document.getElementById("myHeader"),
+	sticky = header.offsetTop;
+function myFunction() {
+	window.pageYOffset > sticky
+		? header.classList.add("sticky")
+		: header.classList.remove("sticky");
+}
+function openTab(e, t) {
+	var n, c, a;
+	for (
+		c = document.getElementsByClassName("tabcontent"), n = 0;
+		n < c.length;
+		n++
+	)
+		c[n].style.display = "none";
+	for (
+		a = document.getElementsByClassName("tablinks"), n = 0;
+		n < a.length;
+		n++
+	)
+		a[n].className = a[n].className.replace(" active", "");
+	(document.getElementById(t).style.display = "block"),
+		(e.currentTarget.className += " active");
+}
+const hamburger = document.querySelector(".hamburger"),
+	navLinks = document.querySelector(".nav-links"),
+	links = document.querySelectorAll(".nav-links li");
+var mylogo = document.getElementById("myLogo");
+hamburger.addEventListener("click", () => {
+	navLinks.classList.toggle("open"),
+		links.forEach((e) => {
+			e.classList.toggle("fade");
+		});
+}),
+	$(document).ready(function () {
+		$("#hamburgerID").on("click", function () {});
+	}),
+	$(".nav-links li a").on("click", function () {
+		$("#hamburgerID").click();
+	});
